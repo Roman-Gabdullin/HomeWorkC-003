44d84e7d0465d6d25ed9f6e8800d12201d6b06b8
Roman_Gabdullin <romaga59@gmail.com>
2022-10-24 00:20:26 +0500

    Решил дополнительную задачу

---
+++ additionally/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дополнительно.
+// Напишите программу, которая принимает на вход число (N) и выдает таблицу простых чисел от 1 до N.
+// 11 -> 2, 3, 5 , 7, 11
+
+Console.WriteLine("Введите натуральное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+void Natur(int num)
+{
+    Console.WriteLine($"{num}->");
+    for (int i = 2; i <= num; i++)
+    {
+        if (i == 2 || i ==3)
+            Console.WriteLine($"    {i}");
+        if (i % 2 != 0 && i % 3 !=0)
+            Console.WriteLine($"    {i}");
+    }
+}
+Natur(n);
